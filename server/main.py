3caabde8aae773310bba56ef6bd5a559a2ed0e1c
tlebrize <lebrize.t@gmail.com>
2021-05-26 17:49:56 +0200

    cors + simpler settings

--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import episode, attribute, auth
 from .settings import Settings
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=Settings.ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(episode)
 app.include_router(attribute)
--- server/settings.py
+++ server/settings.py
@@ -1,7 +1,8 @@
-import os
-
-
 class Settings:
-    HOST = os.environ.get("HOST", "http://localhost:8000")
-    SECRET_KEY = os.environ.get("SECRET_KEY", "secr3t")
-    DEBUG = os.environ.get("DEBUG", "True") == "True"
+    HOST = "http://localhost:8000"
+    SECRET_KEY = "secr3t"
+    DEBUG = True
+    ORIGINS = [
+        "http://localhost:8000/",
+        "http://localhost:8080/",
+    ]
