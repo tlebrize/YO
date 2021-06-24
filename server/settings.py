2ca623148f2c57f75c3eef2276817f61068cc476
Adèle Gaudry <agaudry@student.42.fr>
2021-06-24 19:16:18 +0200

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -2,5 +2,11 @@ class Settings:
     HOST = "http://localhost:8000"
     SECRET_KEY = "secr3t"
     DEBUG = True
-    ORIGINS = ["*"]
+    ORIGINS = [
+        "http://localhost:8000",
+        "http://localhost:8080",
+        "http://127.0.0.1:8000",
+        "http://127.0.0.1:8080",
+        "https://yo.theof.fr",
+    ]
     DB_URL = "postgres://yo:securepasswd@0.0.0.0:5432/yo"
